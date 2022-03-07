eb9c26b69140fba91a8696f32c94838c3e77b43a
Arthur Kim <kimdwkimdw@gmail.com>
2022-03-07 15:34:45 +0900

    Add Usage

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Usage
+
+```
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/kimdwkimdw/robert/master/robert.sh)"
+```
--- robert.sh
+++ robert.sh
@@ -1,1 +1,1 @@
-bash <(curl -s https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat) <(curl -sL https://github.com/kimdwkimdw/robert/raw/master/$((RANDOM % 2)).jpg)
+bash <(curl -s https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat) <(curl -sL https://raw.githubusercontent.com/kimdwkimdw/robert/master/$((RANDOM % 2)).jpg
