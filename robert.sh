f0394f5312c53dbe2d5c826ea4852c7629d7b241
Arthur Kim <kimdwkimdw@gmail.com>
2022-03-07 15:39:02 +0900

    Fix imgcat

--- robert.sh
+++ robert.sh
@@ -1,1 +1,2 @@
-bash <(curl -s https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat) <(curl -sL https://raw.githubusercontent.com/kimdwkimdw/robert/master/$((RANDOM % 2)).jpg
+#!/bin/bash
+bash <(curl -s https://iterm2.com/utilities/imgcat) <(curl -sL https://raw.githubusercontent.com/kimdwkimdw/robert/master/$((RANDOM % 2)).jpg)
