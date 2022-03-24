bdf419f33068208610becbdc0ab5db2abc33a849
somniumism <lsm.somniator@gmail.com>
2022-03-24 14:37:34 +0900

    Add robert's picture; Don't talk to me

--- robert.sh
+++ robert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash <(curl -s https://iterm2.com/utilities/imgcat) <(curl -sL https://raw.githubusercontent.com/kimdwkimdw/robert/master/$((RANDOM % 2)).jpg)
+bash <(curl -s https://iterm2.com/utilities/imgcat) <(curl -sL https://raw.githubusercontent.com/kimdwkimdw/robert/master/$((RANDOM % 3)).jpg)
